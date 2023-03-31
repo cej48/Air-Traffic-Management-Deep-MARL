d44b7f78f6df5758d6c34c82893d43e851466459
cj <chris@jonesey.net>
2023-03-31 11:44:26 +0100

    broken td3

--- TD3.py
+++ TD3.py
@@ -10,6 +10,7 @@ from distutils.util import strtobool
 import sys
 sys.path.append("build/")
 import PyATMSim
+from random import randint
 from stable_baselines3.common.buffers import ReplayBuffer
 import gym
 
@@ -55,13 +56,13 @@ def parse_args():
         help="the discount factor gamma")
     parser.add_argument("--tau", type=float, default=0.005,
         help="target smoothing coefficient (default: 0.005)")
-    parser.add_argument("--batch-size", type=int, default=256,
+    parser.add_argument("--batch-size", type=int, default=128,
         help="the batch size of sample from the reply memory")
     parser.add_argument("--policy-noise", type=float, default=0.2,
         help="the scale of policy noise")
     parser.add_argument("--exploration-noise", type=float, default=0.2,
         help="the scale of exploration noise")
-    parser.add_argument("--learning-starts", type=int, default=10e3,
+    parser.add_argument("--learning-starts", type=int, default=50e3,
         help="timestep to start learning")
     parser.add_argument("--policy-frequency", type=int, default=2,
         help="the frequency of training policy (delayed)")
@@ -76,9 +77,9 @@ def parse_args():
 class QNetwork(nn.Module):
     def __init__(self, env):
         super().__init__()
-        self.fc1 = nn.Linear(np.array(env.single_observation_space.shape).prod() + np.prod(env.single_action_space.shape), 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 1)
+        self.fc1 = nn.Linear(np.array(env.single_observation_space.shape).prod() + np.prod(env.single_action_space.shape), 512)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 1)
 
     def forward(self, x, a):
         x = torch.cat([x, a], 1)
@@ -91,9 +92,9 @@ class QNetwork(nn.Module):
 class Actor(nn.Module):
     def __init__(self, env):
         super().__init__()
-        self.fc1 = nn.Linear(np.array(env.single_observation_space.shape).prod(), 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.fc_mu = nn.Linear(256, np.prod(env.single_action_space.shape))
+        self.fc1 = nn.Linear(np.array(env.single_observation_space.shape).prod(), 512)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc_mu = nn.Linear(512, np.prod(env.single_action_space.shape))
         # action rescaling
         self.register_buffer(
             "action_scale", torch.tensor((env.action_space.high - env.action_space.low) / 2.0, dtype=torch.float32)
@@ -157,13 +158,13 @@ if __name__ == "__main__":
     envs.single_observation_space.dtype = np.float32
     #def __init__(self, capacity, batch_size, buffer_ready, states, actions):
     # rb = ReplayBuffer(args.buffer_size, args.batch_size, 1000, envs.observation_space.shape, envs.action_space.shape)
-    rb = ReplayBuffer(
+    buffers = [ReplayBuffer(
         args.buffer_size,
         envs.single_observation_space,
         envs.single_action_space,
         device,
-        handle_timeout_termination=False,
-    )
+        handle_timeout_termination=False,)
+        for x in range(25)]
     start_time = time.time()
 
     # TRY NOT TO MODIFY: start the game
@@ -173,13 +174,23 @@ if __name__ == "__main__":
     actions = {i : [] for i in envs.env.traffic}
     rewards = {i : 0 for i in envs.env.traffic}
     terminated = {i : False for i in envs.env.traffic}
+    buffer_index = {envs.env.traffic[i] : i for i in range(0, len(envs.env.traffic))}
+    frees = []
 
     noise = torch.normal(0, actor.action_scale * args.exploration_noise)
     # for each step
     for global_step in range(args.total_timesteps):
         # ALGO LOGIC: put action logic here
         new_state = {}
+        for traffic in list(buffer_index):
+            if traffic not in envs.env.traffic:
+                frees.append(buffer_index[traffic])
+                buffer_index.pop(traffic)
+
+
         for traffic in envs.env.traffic:
+            if traffic not in buffer_index:
+                buffer_index[traffic] = frees.pop()
             if not traffic.terminated:
                 if traffic in states:
                     new_state[traffic] = states[traffic] 
@@ -215,7 +226,7 @@ if __name__ == "__main__":
             terminated[traffic] = traffic.terminated
 
         for traffic in states:
-            rewards[traffic] = (45 + traffic.reward)/10
+            rewards[traffic] = (45 + traffic.reward)
 
         observation = {i : i.get_observation() for i in envs.env.traffic}
         # TRY NOT TO MODIFY: save data to reply buffer; handle `terminal_observation`
@@ -225,7 +236,7 @@ if __name__ == "__main__":
             # print("ACTION: " +str ( actions[traffic]))
             # print("REWARDS: " +str ( rewards[traffic]))
             # print("TERMINATED: " + str (terminated[traffic]))
-            rb.add(states[traffic], observation[traffic], actions[traffic], rewards[traffic], terminated[traffic], [])
+            buffers[buffer_index[traffic]].add(states[traffic], observation[traffic], actions[traffic], rewards[traffic], terminated[traffic], [])
 
         
         if final_terminated:
@@ -241,54 +252,55 @@ if __name__ == "__main__":
         states = observation
         # ALGO LOGIC: training.
         if global_step > args.learning_starts:
-            data = rb.sample(args.batch_size)
-            with torch.no_grad():
-                clipped_noise = (torch.randn_like(data.actions, device=device) * args.policy_noise).clamp(
-                    -args.noise_clip, args.noise_clip
-                ) * target_actor.action_scale
+            for buffer in buffers:
+                data = buffer.sample(args.batch_size)
+                with torch.no_grad():
+                    clipped_noise = (torch.randn_like(data.actions, device=device) * args.policy_noise).clamp(
+                        -args.noise_clip, args.noise_clip
+                    ) * target_actor.action_scale
 
-                next_state_actions = (target_actor(data.next_observations) + clipped_noise).clamp(
-                    envs.single_action_space.low[0], envs.single_action_space.high[0]
-                )
-                qf1_next_target = qf1_target(data.next_observations, next_state_actions)
-                qf2_next_target = qf2_target(data.next_observations, next_state_actions)
-                min_qf_next_target = torch.min(qf1_next_target, qf2_next_target)
-                next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (min_qf_next_target).view(-1)
+                    next_state_actions = (target_actor(data.next_observations) + clipped_noise).clamp(
+                        envs.single_action_space.low[0], envs.single_action_space.high[0]
+                    )
+                    qf1_next_target = qf1_target(data.next_observations, next_state_actions)
+                    qf2_next_target = qf2_target(data.next_observations, next_state_actions)
+                    min_qf_next_target = torch.min(qf1_next_target, qf2_next_target)
+                    next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (min_qf_next_target).view(-1)
 
-            qf1_a_values = qf1(data.observations, data.actions).view(-1)
-            qf2_a_values = qf2(data.observations, data.actions).view(-1)
-            qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
-            qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
-            qf_loss = qf1_loss + qf2_loss
+                qf1_a_values = qf1(data.observations, data.actions).view(-1)
+                qf2_a_values = qf2(data.observations, data.actions).view(-1)
+                qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
+                qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
+                qf_loss = qf1_loss + qf2_loss
 
-            # optimize the model
-            q_optimizer.zero_grad()
-            qf_loss.backward()
-            q_optimizer.step()
+                # optimize the model
+                q_optimizer.zero_grad()
+                qf_loss.backward()
+                q_optimizer.step()
 
-            if global_step % args.policy_frequency == 0:
-                actor_loss = -qf1(data.observations, actor(data.observations)).mean()
-                actor_optimizer.zero_grad()
-                actor_loss.backward()
-                actor_optimizer.step()
+                if global_step % args.policy_frequency == 0:
+                    actor_loss = -qf1(data.observations, actor(data.observations)).mean()
+                    actor_optimizer.zero_grad()
+                    actor_loss.backward()
+                    actor_optimizer.step()
 
-                # update the target network
-                for param, target_param in zip(actor.parameters(), target_actor.parameters()):
-                    target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
-                for param, target_param in zip(qf1.parameters(), qf1_target.parameters()):
-                    target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
-                for param, target_param in zip(qf2.parameters(), qf2_target.parameters()):
-                    target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+                    # update the target network
+                    for param, target_param in zip(actor.parameters(), target_actor.parameters()):
+                        target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+                    for param, target_param in zip(qf1.parameters(), qf1_target.parameters()):
+                        target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+                    for param, target_param in zip(qf2.parameters(), qf2_target.parameters()):
+                        target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
 
-            if global_step % 100 == 0:
-                writer.add_scalar("losses/qf1_values", qf1_a_values.mean().item(), global_step)
-                writer.add_scalar("losses/qf2_values", qf2_a_values.mean().item(), global_step)
-                writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
-                writer.add_scalar("losses/qf2_loss", qf2_loss.item(), global_step)
-                writer.add_scalar("losses/qf_loss", qf_loss.item() / 2.0, global_step)
-                writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
-                print("SPS:", int(global_step / (time.time() - start_time)))
-                writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+                if global_step % 100 == 0:
+                    writer.add_scalar("losses/qf1_values", qf1_a_values.mean().item(), global_step)
+                    writer.add_scalar("losses/qf2_values", qf2_a_values.mean().item(), global_step)
+                    writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
+                    writer.add_scalar("losses/qf2_loss", qf2_loss.item(), global_step)
+                    writer.add_scalar("losses/qf_loss", qf_loss.item() / 2.0, global_step)
+                    writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
+                    print("SPS:", int(global_step / (time.time() - start_time)))
+                    writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
 
 
     envs.close()
